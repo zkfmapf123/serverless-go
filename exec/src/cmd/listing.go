e2e48f3f0edcac95a1c5052dc51b3c8de34660bc
zkfmapf123 <zkfmapf123@github.com>
2024-01-28 15:35:40 +0900

    Add List

--- exec/src/cmd/listing.go
+++ exec/src/cmd/listing.go
@@ -1,7 +1,14 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/zkfmapf123/serverless-go-deploy-agent/src/aws"
+	"github.com/zkfmapf123/serverless-go-deploy-agent/src/filesystem"
 )
 
 var listingCmd = &cobra.Command{
@@ -10,6 +17,20 @@ var listingCmd = &cobra.Command{
 	Long:  "List Lambda",
 	Run: func(cmd *cobra.Command, args []string) {
 
+		cfg := aws.New(viper.GetString("profile"))
+		list := cfg.GetLambdaList()
+		if len(list) == 0 {
+			fmt.Println("Lambda List is Not Exists")
+			os.Exit(0)
+		}
+
+		filesystem.PrintTable[aws.LambdaInfo](list, []string{"Name", "Desc", "Env", "Size", "Last Updated"},
+			func(k string, v aws.LambdaInfo) []string {
+				envString := strconv.Itoa(v.Env)
+				sizeString := strconv.Itoa(int(v.Size))
+
+				return []string{k, v.Desc, envString, sizeString, v.LastUpdated}
+			})
 	},
 }
 
